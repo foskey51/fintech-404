429a1c5fc181ca108a5e97d65d179ea83a1d0a81
foskey51 <dhanushhs51@gmail.com>
2025-04-11 20:37:16 +0530

    add(ui): added hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react'
+import "./App.css"
+import Hero from './components/Hero';
 
-const App = () =>{
-  return(
+const App = () => {
+  return (
     <>
-    
+      <div className='flex h-full w-full'>
+        <Hero />
+      </div>
     </>
-  )
+  ) 
 }
+
+export default App;
